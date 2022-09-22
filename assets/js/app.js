cacc956a1343c173503c655fc321f34075669d19
Smak0001 <smak0001@hz.nl>
2022-09-22 12:40:26 +0200

    Test Commit Xander

---
+++ assets/js/app.js
@@ -0,0 +1,7 @@
+// Prove that JavaScript is working in your browser. You may delete this.
+console.log('JavaScript is working!');
+let personsname = window.prompt("What's your name")
+let inputminutes = window.prompt("Fill your minutes in that you want to convert")
+const message = "Hello " + personsname + ", Your input was " + inputminutes + " minutes";
+document.write(message);
+
