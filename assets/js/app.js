f0885ee947b2480cea825a84025789bd983e0014
Smak0001 <smak0001@hz.nl>
2022-09-23 20:11:43 +0200

    Done

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,12 +1,12 @@
 // Prove that JavaScript is working in your browser. You may delete this.
 console.log('JavaScript is working!');
-let personsname = window.prompt("What's your name")
-let inputminutes = window.prompt("Fill your minutes in that you want to convert")
+const personsname = window.prompt("What's your name");
+const inputminutes = window.prompt("Fill your minutes in that you want to convert");
 const message = "Hello " + personsname + ", Your input was " + inputminutes + " minutes";
-let hours = (inputminutes / 60);
-let rhours = Math.floor(hours);
-let minutes = (hours - rhours) * 60;
-let rminutes = Math.round(minutes);
+const hours = (inputminutes / 60);
+const rhours = Math.floor(hours);
+const minutes = (hours - rhours) * 60;
+const rminutes = Math.round(minutes);
 document.write(message);
-document.write("<br>,<br>");
+document.write("<br><br>");
 document.write("<text align=center>Output: " + rhours + " hours" + " and " + rminutes + " minutes");
