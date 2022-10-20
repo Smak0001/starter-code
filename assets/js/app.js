9b279a153b32ef0e54fc339701635ede01080df1
Smak0001 <smak0001@hz.nl>
2022-10-20 13:00:49 +0200

    Done

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,8 +1,6 @@
-// Prove that JavaScript is working in your browser. You may delete this.
-console.log('JavaScript is working!');
 const personsname = window.prompt("What's your name");
 const inputminutes = window.prompt("Fill your minutes in that you want to convert");
-const message = "Hello " + personsname + ", Your input was " + inputminutes + " minutes";
+const message = "Hello " + username + ", Your input was " + inputminutes + " minutes";
 const hours = (inputminutes / 60);
 const rhours = Math.floor(hours);
 const minutes = (hours - rhours) * 60;
